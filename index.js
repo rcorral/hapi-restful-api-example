88ce699293f1c403639bd3ba0954c5d5a84ed86d
Rafael Corral <rxcorral@gmail.com>
2014-11-13 22:24:13 -0800

    Adding npm-good for logging.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+var Good = require('good');
 var Hapi = require('hapi');
+
 var server = new Hapi.Server('localhost', 8000);
 
 server.route({
@@ -10,4 +12,20 @@ server.route({
     }
 });
 
-server.start();
+server.pack.register({
+    plugin: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            args:[{log: '*', request: '*', error: '*'}]
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
